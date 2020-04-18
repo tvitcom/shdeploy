715ab4edbe23f7e4b2bc1825bef26bf01d19c522
tvitcom <tvitcom@yandex.ru>
2020-04-18 23:05:24 +0300

    add yes responce on google-chrome installation rquest

--- pub/deb9/install.sh
+++ pub/deb9/install.sh
@@ -101,7 +101,7 @@ apt-get -y install meld mysql-workbench filezilla chromium
 
 ## google-chrom
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install ./google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
 
 ## command developer soft
 apt-get -y install gcc make linux-headers-amd64
