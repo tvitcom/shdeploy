d6fa9c99ca1fb162280ab24627405877e7a9f6d6
tvitcom <tvitcom@yandex.ru>
2020-07-26 14:01:34 +0300

    minor fixes

--- pub/deb10/install.sh
+++ pub/deb10/install.sh
@@ -391,7 +391,7 @@ echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.clou
 #OR: echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 #OR: curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-apt-get update && apt-get install google-cloud-sdk
+apt-get update && apt-get -y install google-cloud-sdk
 
 ## TODO:tenzorflow
 
